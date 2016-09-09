9a6bb0bb8e73bae799174ed50e691c9ee026a2c5
Vitalii Doroshenko <vetalpro.exe@gmail.com>
2016-09-09 22:38:08 +0300

    create deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
+    runSequence = require('run-sequence'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
@@ -11,11 +12,29 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-clean-css'),
+    del = require('del'),
     wiredep = require('wiredep').stream;
 
+
+//------------------------PATHS----------------------------------
+var bases = {
+  app: 'app/',
+  dist: 'dist/',
+};
+
+var paths = {
+  scripts: ['js/**/*.js'],
+  styles: ['css/**/*.css'],
+  html: ['index.html'],
+  images: ['resource/img/**/*.png', 'resource/img/**/*.jpg'],
+  fonts: ['resource/fonts/**/*'],
+  icons: ['resource/icons/**/*']
+};
+
 //----------------- SASS -------------------------
+
 gulp.task('sass', function () {
-  return gulp.src('./app/scss/*.scss')
+  return gulp.src(bases.app + 'scss/*.scss')
       .pipe(sourcemaps.init())
       .pipe(sass({
         outputStyle: 'expanded'
@@ -24,7 +43,7 @@ gulp.task('sass', function () {
         browsers: ['last 4 versions']
       }))
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./app/css'))
+      .pipe(gulp.dest(bases.app + 'css/'))
       .pipe(browserSync.stream({match: '**/*.css'}));           //reload page with browsersync
 });
 
@@ -32,16 +51,16 @@ gulp.task('sass', function () {
 //-------------------------jade(+wiredep)-------------------------
 
 gulp.task('jade', function () {
-  gulp.src('./app/jade/*.pug')
+  return gulp.src(bases.app + 'jade/*.pug')
       .pipe(pug({
         pretty: true
       }))
-      .pipe(gulp.dest('./app'))
+      .pipe(gulp.dest(bases.app))
       .pipe(wiredep({
-        direcory: './app/bower_components'
+        direcory: bases.app + 'bower_components/'
       }))
-      .pipe(gulp.dest('./app'))
-      .pipe(browserSync.stream());
+      .pipe(gulp.dest(bases.app))
+      .pipe(browserSync.stream());  //reload page
 
 });
 
@@ -56,26 +75,16 @@ gulp.task('jade', function () {
 // });
 
 
-//-------------------------useref (сборка в dist)-------------------------
-
-gulp.task('dist', function () {
-  return gulp.src('app/*.html')
-      .pipe(useref())
-      .pipe(gulpif('*.js', uglify()))
-      .pipe(gulpif('*.css', minifyCss()))
-      .pipe(gulp.dest('dist'));
-});
-
 //---------------------------BROWSER-SYNC----------------------
 
-function hello(){
+function hello() {
   console.log('Ready for work ;)');
 }
 
 gulp.task('serve', function () {
   browserSync.init({
     server: {
-      baseDir: "./app"
+      baseDir: bases.app
     },
     // proxy: "localhost:8888"
     // files: ["app/*.html", "app/css/*.css", "app/js/*.js"]
@@ -83,13 +92,53 @@ gulp.task('serve', function () {
     // logConnections: true,
     open: 'local',
     notify: true
-  },hello);
-  gulp.watch('./app/scss/**/*.scss', ['sass']);
-  gulp.watch('./app/jade/**/*.pug', ['jade']);
+  }, hello);
+  gulp.watch(bases.app + 'scss/**/*.scss', ['sass']);
+  gulp.watch(bases.app + 'jade/**/*.pug', ['jade']);
   // browserSync.watch("*.html").on("change", browserSync.reload);
 
 
 });
 
-gulp.task('default', ['sass', 'jade', 'serve']);
 
+//============================== DEPLOY =====================================
+
+//useref (сборка в dist)
+gulp.task('dist', function () {
+  return gulp.src(bases.app + '*.html')
+      .pipe(useref())
+      .pipe(gulpif('*.js', uglify()))
+      .pipe(gulpif('*.css', minifyCss()))
+      .pipe(gulp.dest(bases.dist));
+});
+
+// Delete the dist directory
+gulp.task('clean', function () {
+  return del.sync(bases.dist);
+});
+
+// Imagemin images and ouput them in dist
+gulp.task('copyImg', function () {
+  gulp.src(paths.images, {cwd: bases.app})
+  // .pipe(imagemin())
+      .pipe(gulp.dest(bases.dist + 'resource/img/'));
+});
+
+gulp.task('copyFonts', function () {
+  gulp.src(paths.fonts, {cwd: bases.app})
+      .pipe(gulp.dest(bases.dist + 'resource/fonts/'));
+});
+
+gulp.task('copyIcons', function () {
+  gulp.src(paths.icons, {cwd: bases.app})
+      .pipe(gulp.dest(bases.dist + 'resource/icons/'));
+});
+
+gulp.task('deploy', ['clean', 'copyImg', 'copyFonts', 'copyIcons', 'dist']);
+
+//==================================================================
+
+
+gulp.task('default', function () {
+  runSequence(['sass','jade'], 'serve');
+});
