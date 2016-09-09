a90b036f07b8bea063cc3b91a9406e9118fe5449
Vitalii Doroshenko <vetalpro.exe@gmail.com>
2016-09-09 13:34:49 +0300

    small fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,40 @@
 var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug');
 
 gulp.task('sass', function () {
-    return gulp.src('./source/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 3 versions']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./source/css'));
+  return gulp.src('./source/scss/**/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        outputStyle: 'expanded'
+      }).on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+      }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./source/css'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./source/scss/**/*.scss', ['sass']);
+  gulp.watch('./source/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('jade', function () {
+  return gulp.src('./source/*.pug')
+      .pipe(pug({
+        pretty: true
+      }))
+      .pipe(gulp.dest('./source/'));
+});
+
+gulp.task('jade:watch', function () {
+  gulp.watch('./source/**/*.pug', ['jade']);
 });
 
 
-
-gulp.task('default',function(){
+gulp.task('default', function () {
 
 });
 
